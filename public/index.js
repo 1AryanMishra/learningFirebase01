53a001be1b741ddb74eb1ee29142c79b662099df
Aryan Mishra <mishra321123@gmail.com>
2021-09-01 10:57:56 +0530

    setup done

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
-import { getAuth, onAuthStateChange, signInWithPopop, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js'
+import { getAuth, signOut, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js'
+
+var provider_btn = document.querySelector("#provider_btn");
+var auth_msg = document.querySelector("#auth_msg");
+var user_name = document.querySelector("#name");
 
 const config = {
     apiKey: "AIzaSyCiUPzj8pODh1yxkRG-0b8aw4PIf8FLnjQ",
@@ -23,3 +27,30 @@ const auth = getAuth(firebaseApp);
 const providerGoogle = new GoogleAuthProvider();
 
 
+
+ var logged_in = false;
+provider_btn.addEventListener("click", () => {
+    if(logged_in){
+        signOut(auth).then(()=>{
+            auth_msg.textContent = "Sign In with Google";
+            data.textContent = "Hi User!, Please Sign In with Google to see your data.";
+            logged_in = false;
+        }).catch((error) => {
+            auth_msg.textContent = "An error happend. Please try again.";
+            console.log(error);
+        });
+    }
+    else{
+        signInWithPopup(auth, providerGoogle).then(
+            (result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                data.textContent = `${result.user}`;
+                auth_msg.textContent = "Sign Out";
+                logged_in = true;
+            }).catch((error) => {
+                auth_msg.textContent = "An error occurred while Sign In. Please try again.";
+                console.log(error);
+            }
+        );
+    }
+})
